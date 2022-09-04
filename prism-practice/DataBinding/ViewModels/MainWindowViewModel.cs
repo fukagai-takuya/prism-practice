7f4c41d1a01cc057291a00a7b87d766b2e4434ed
Takuya Fukagai <fukagai@leafwindow.com>
2022-09-04 13:29:43 +0900

    first commit

---
+++ prism-practice/DataBinding/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace DataBinding.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism DataBinding";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _dateTimeLabel = System.DateTime.Now.ToString();
+        public string DateTimeLabel
+        {
+            get { return _dateTimeLabel; }
+            set { SetProperty(ref _dateTimeLabel, value); }
+        }
+
+        public DelegateCommand DateTimeUpdateButton { get; }
+
+        public MainWindowViewModel()
+        {
+            DateTimeUpdateButton = new DelegateCommand(DateTimeUpdateButtonExecute);
+        }
+
+        private void DateTimeUpdateButtonExecute()
+        {
+            DateTimeLabel = System.DateTime.Now.ToString();
+        }
+    }
+}
